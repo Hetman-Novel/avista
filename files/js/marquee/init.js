4eae2367179acaa2d0fb8c5626d755b6fa6259b8
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2025-08-02 02:28:18 +0300

    Add files via upload

---
+++ files/js/marquee/init.js
@@ -0,0 +1,11 @@
+$(function() {
+   $('.marquee').marquee({
+      duration: 20000,
+      startVisible: true,
+      duplicated: true,
+      duplicateCount: 150,
+      //gap: 50,
+      //speed: 15,
+      //pauseOnHover: true,
+   });
+});
